02f6829e57242f803b80d50a606909147b947bd0
Esakkinathan <esakkibes91@gmail.com>
2025-03-02 16:16:56 +0530

    changes in dataset included responses

---
+++ summa_kili.py
@@ -0,0 +1,28 @@
+from transformers import T5ForConditionalGeneration,RobertaTokenizer
+import pandas as pd
+
+
+# Load the trained model and tokenizer
+model_dir = "./model/darla-model"
+tokenizer = RobertaTokenizer.from_pretrained(model_dir)
+model = T5ForConditionalGeneration.from_pretrained(model_dir)
+
+# Function to make predictions
+def predict_bash_command(nl_command):
+    nl_command = "translate english to bash: " + nl_command
+    # Preprocess the input command
+    input_ids = tokenizer.encode(nl_command, return_tensors="pt", max_length=128, truncation=True)
+
+    # Generate prediction
+    output_ids = model.generate(input_ids, max_length=128, num_beams=4, early_stopping=True)
+
+    # Decode the predicted Bash command
+    bash_command = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    return bash_command
+
+while True:
+    a = input("Enter NL Command or e to exit: ")
+    if a == 'e':
+        break
+    print(predict_bash_command(a))
+
